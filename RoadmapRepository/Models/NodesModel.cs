1a7617e759efde8affa352a4ae641b2987c9faf2
Arthuthu <arthurgeromello@hotmail.com>
2023-01-23 17:06:27 -0300

    Models folder added

---
+++ RoadmapRepository/Models/NodesModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadmapRepository.Models;
+
+public class NodesModel
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; }
+	public string? Description { get; set; }
+
+
+	public RoadmapModel RoadMap { get; set; }
+}
---
+++ RoadmapRepository/Models/RoadmapModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadmapRepository.Models;
+
+public class RoadmapModel
+{
+	public Guid Id { get; set; }
+	public string? Description { get; set; }
+
+
+	public List<NodesModel> Nodes { get; set; }
+	public UserModel User { get; set; }
+}
