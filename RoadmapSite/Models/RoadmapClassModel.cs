cb7d849f01fce0cb3c21eb6f30dc078d3279a85a
Arthuthu <arthurgeromello@hotmail.com>
2023-02-13 15:20:03 -0300

    Profile pages shows user profile

--- RoadmapSite/Models/RoadmapClassModel.cs
+++ RoadmapSite/Models/RoadmapClassModel.cs
@@ -6,9 +6,13 @@ public class RoadmapClassModel
 	public string Name { get; set; }
 	public string Description { get; set; }
 	public string Category { get; set; }
+
+	public string AuthorId { get; set; }
 	public string Author { get; set; }
-	public string RoadmapClass { get; set; }
-	public int RoadmapVotes { get; set; }
+
+
+	public string RoadmapHtmlClass { get; set; }
+	public int RoadmapTotalVotes { get; set; }
 
 
 	public Guid UserId { get; set; }
