ee4134f12f183d6bf7e95e341e522b2d5d46029a
Arthuthu <arthurgeromello@hotmail.com>
2023-03-06 16:15:48 -0300

    Sort by Newset and Popularity buttons added - Removed verifica autenticacao

--- RoadmapAPIApp/Response/RoadmapClassResponse.cs
+++ RoadmapAPIApp/Response/RoadmapClassResponse.cs
@@ -6,6 +6,7 @@ public class RoadmapClassResponse
 	public string Name { get; set; }
 	public string? Description { get; set; }
 	public string? Category { get; set; }
+	public DateTime CreatedDate { get; set; }
 
 	public Guid UserId { get; set; }
 }
--- RoadmapSite/Models/RoadmapClassModel.cs
+++ RoadmapSite/Models/RoadmapClassModel.cs
@@ -6,6 +6,7 @@ public class RoadmapClassModel
 	public string? Name { get; set; }
 	public string? Description { get; set; }
 	public string? Category { get; set; }
+	public DateTime CreatedDate { get; set; }
 				 
 	public string? AuthorId { get; set; }
 	public string? Author { get; set; }
