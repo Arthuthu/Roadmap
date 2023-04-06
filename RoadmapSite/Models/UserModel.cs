d6afb680b6cc777324598c32d09d557dcb4a23e5
Arthuthu <arthurgeromello@hotmail.com>
2023-04-06 17:20:23 -0300

    Users can get unbanned

--- RoadmapSite/Models/UserModel.cs
+++ RoadmapSite/Models/UserModel.cs
@@ -6,6 +6,6 @@ public class UserModel
 	public string Username { get; set; }
 	public string? Password { get; set; }
 	public string? Bio { get; set; }
-	public string IsAdmin { get; set; }
-	public string IsBanned { get; set; }
+	public string? IsAdmin { get; set; }
+	public string? IsBanned { get; set; }
 }
