c9c9e29e3cbe9587e094e70701b13d8a9a122d91
Arthuthu <arthurgeromello@hotmail.com>
2023-05-09 17:33:43 -0300

    UnitTests para ComentaroService adicionados

--- RoadmapAPITests/Service/ComentarioServiceTests.cs
+++ RoadmapAPITests/Service/ComentarioServiceTests.cs
@@ -27,7 +27,7 @@ public class ComentarioServiceTests
 		var expectedComentarios = new List<ComentarioModel>()
 		{
 			new ComentarioModel { Id = Guid.NewGuid(), Description = "First Comment" },
-			new ComentarioModel { Id = Guid.NewGuid(), Description = "Second Comment"}
+			new ComentarioModel { Id = Guid.NewGuid(), Description = "Second Comment" }
 		};
 
 		_comentarioRepository.GetAllComentarios(roadmapId).Returns(expectedComentarios);
@@ -44,6 +44,127 @@ public class ComentarioServiceTests
 	[Fact]
 	public async Task GetAllComentarios_ShouldReturnEmptyList_WhenThereAreNoComentarios()
 	{
+		//Arrange
+		Guid roadmapId = Guid.NewGuid();
 
+		var expectedComentarios = Enumerable.Empty<ComentarioModel>();
+		_comentarioRepository.GetAllComentarios(roadmapId).Returns(expectedComentarios);
+
+		//Act
+		var result = await _sut.GetAllComentarios(roadmapId);
+
+		//Assert
+		result.Should().NotBeNull().And.BeEmpty();
+	}
+
+	//GetComentarioById
+	[Fact]
+	public async Task GetComentarioById_ShoudlReturnComentario_WhenComentarioExists()
+	{
+		//Arrange
+		Guid comentarioId = Guid.NewGuid();
+
+		ComentarioModel? expectedComentario = _fixture.Build<ComentarioModel>()
+			.With(x => x.Id, comentarioId)
+			.With(x => x.Description, "Boa chefe")
+			.Create();
+
+		_comentarioRepository.GetComentarioById(comentarioId).Returns(expectedComentario);
+
+		//Act
+		var result = await _sut.GetComentarioById(comentarioId);
+
+		//Assert
+		result.Should().NotBeNull()
+			.And.BeOfType<ComentarioModel>()
+			.And.BeSameAs(expectedComentario);
+	}
+
+	[Fact]
+	public async Task GetComentarioById_ShouldReturnNull_WhenThereAreNoComentariosWithGivenId()
+	{
+		//Arrange
+		Guid comentarioId = Guid.NewGuid();
+		ComentarioModel? comentario = null;
+
+		_comentarioRepository.GetComentarioById(comentarioId).Returns(comentario);
+
+		//Act
+		var result = await _sut.GetComentarioById(comentarioId);
+
+		//Assert
+		result.Should().BeNull();
+	}
+
+	//CreateComentario
+	[Fact]
+	public async Task CreateComentario_ShouldCreateComentario_WhenComentarioIsValid()
+	{
+		//Arrange
+		var comentario = _fixture.Build<ComentarioModel>()
+			.With(x => x.Id, Guid.NewGuid())
+			.With(x => x.Description, "Boa primo")
+			.Create();
+
+		_comentarioRepository.CreateComentario(Arg.Any<ComentarioModel>())
+			.Returns(Task.CompletedTask);
+
+		//Act
+		await _sut.CreateComentario(comentario);
+
+		//Assert
+		await _comentarioRepository.Received(1)
+			.CreateComentario(Arg.Is<ComentarioModel>(c => c.Id != Guid.Empty));
+	}
+
+	//UpdateComentario
+	[Fact]
+	public async Task UpdateComentario_ShouldCallUpdateComentarioMethodOfRepository_WhenValidComentarioIsProvided()
+	{
+		//Arrange
+		var comentario = _fixture.Build<ComentarioModel>()
+			.With(x => x.Id, Guid.NewGuid)
+			.With(x => x.Description, "Boa cousin")
+			.With(x => x.UpdatedDate, DateTime.UtcNow.AddHours(-3))
+			.Create();
+
+		//Act
+		await _sut.UpdateComentario(comentario);
+
+		//Assert
+		await _comentarioRepository.Received(1).UpdateComentario(comentario);
+		comentario.UpdatedDate.Should().BeCloseTo(DateTime.UtcNow.AddHours(-3), TimeSpan.FromSeconds(1));
+	}
+
+	//DeleteAllUserComentarios
+	[Fact]
+	public async Task DeleteAllUserComentarios_ShouldSendDataToRepository_WhenMethodIsCalled()
+	{
+		//Arrange
+		Guid userId = Guid.NewGuid();
+
+		_comentarioRepository.DeleteAllUserComentarios(userId).Returns(Task.CompletedTask);
+
+		//Act
+		await _comentarioRepository.DeleteAllUserComentarios(userId);
+
+		//Assert
+		await _comentarioRepository.Received(1).DeleteAllUserComentarios(userId);
+	}
+
+	//DeleteComentario
+	[Fact]
+	public async Task DeleteComentario_ShouldSendDataToRepository_WhenMethodIsCalled()
+	{
+		//Arrange
+		Guid cometanrioId = Guid.NewGuid();
+
+		_comentarioRepository.DeleteComentario(cometanrioId).Returns(Task.CompletedTask);
+
+		//Act
+		await _comentarioRepository.DeleteComentario(cometanrioId);
+
+		//Assert
+		await _comentarioRepository.Received(1).DeleteComentario(cometanrioId);
 	}
 }
