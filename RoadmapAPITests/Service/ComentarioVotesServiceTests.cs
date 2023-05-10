12534965790c9cb26e443541c648dd3829419034
Arthuthu <arthurgeromello@hotmail.com>
2023-05-10 17:31:11 -0300

    Teste unitarios para ComentarioVotesService finalizados

--- RoadmapAPITests/Service/ComentarioVotesServiceTests.cs
+++ RoadmapAPITests/Service/ComentarioVotesServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using AutoFixture;
+using FluentAssertions;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using RoadmapRepository.Interfaces;
 using RoadmapRepository.Models;
 using RoadmapServices.Classes;
@@ -17,11 +19,12 @@ public class ComentarioVotesServiceTests
         _sut = new ComentarioVotesService(_comentarioVotesRepository);
     }
 
+	//GetAllComentarioVotes
 	[Fact]
 	public async Task GetAllComentarioVotes_ShouldReturnListOfComentarioVotes_WhenMethodIsCalled()
 	{
 		// Arrange
-		Guid id = Guid.NewGuid();
+		Guid comentarioId = Guid.NewGuid();
 		Guid userId = Guid.NewGuid();
 		var expectedVotes = new List<ComentarioVotesModel>
 	{
@@ -30,14 +33,94 @@ public class ComentarioVotesServiceTests
 		new ComentarioVotesModel { Id = Guid.NewGuid(), ComentarioId = comentarioId, UserId = userId },
 	};
 
-		_comentarioVotesRepository.GetAllComentarioVotes().Returns(expectedVotes);
+		_comentarioVotesRepository.GetAllComentarioVotes(userId, comentarioId).Returns(expectedVotes);
 
 		// Act
-		var result = await _sut.GetAllComentarioVotes();
+		var result = await _sut.GetAllComentarioVotes(userId, comentarioId);
 
 		// Assert
 		result.Should().NotBeNull();
 		result.Should().HaveCount(expectedVotes.Count);
 		result.Should().BeEquivalentTo(expectedVotes);
 	}
+
+	[Fact]
+	public async Task GetAllComentarioVotes_ShouldReturnEmptyList_WhenThereAreNoVotes()
+	{
+		//Arrange
+		Guid userId = Guid.NewGuid();
+		Guid comentarioId = Guid.NewGuid();
+
+		var expectedComentarioVotes = Enumerable.Empty<ComentarioVotesModel>();
+
+		_comentarioVotesRepository.GetAllComentarioVotes(userId, comentarioId).Returns(expectedComentarioVotes);
+
+		//Act
+		var result = await _sut.GetAllComentarioVotes(userId, comentarioId);
+
+		//Assert
+		result.Should().NotBeNull().And.BeEmpty();
+	}
+
+	//AddComentarioVote
+	[Fact]
+	public async Task AddComentarioVote_ShouldReturnSuccessMessage_WhenVoteIsAdded()
+	{
+		// Arrange
+		Guid userId = Guid.NewGuid();
+		Guid comentarioId = Guid.NewGuid();
+		_comentarioVotesRepository.AddComentarioVote(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>())
+			.Returns(Task.CompletedTask);
+
+		// Act
+		var result = await _sut.AddComentarioVote(userId, comentarioId);
+
+		// Assert
+		result.Should().NotBeNull();
+		result.Should().Be("Voto adicionado com sucesso");
+	}
+
+	[Fact]
+	public async Task AddComentarioVote_ShouldReturnErrorMessage_WhenRepositoryThrowsException()
+	{
+		// Arrange
+		Guid userId = Guid.NewGuid();
+		Guid comentarioId = Guid.NewGuid();
+		_comentarioVotesRepository
+			.When(x => x.AddComentarioVote(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>()))
+			.Throw(new Exception("Repository exception"));
+
+		// Act
+		string result = await _sut.AddComentarioVote(userId, comentarioId);
+
+		// Assert
+		result.Should().NotBeNullOrEmpty();
+		result.Should().Be("Ocorreu um erro ao adicionar o voto");
+	}
+
+	//DeleteComentarioVote
+	[Fact]
+	public async Task DeleteComentarioVote_ShouldDeleteComentarioVote_WhenValidIdIsProvided()
+	{
+		// Arrange
+		Guid id = Guid.NewGuid();
+
+		// Act
+		await _sut.DeleteComentarioVote(id);
+
+		// Assert
+		await _comentarioVotesRepository.Received(1).DeleteComentarioVote(id);
+	}
+
+	[Fact]
+	public async Task DeleteComentarioVote_ShouldThrowException_WhenRepositoryThrowsException()
+	{
+		// Arrange
+		Guid comentarioVoteId = Guid.NewGuid();
+		_comentarioVotesRepository.DeleteComentarioVote(comentarioVoteId).Throws(new Exception("Database connection failed"));
+
+		// Act and Assert
+		await Assert.ThrowsAsync<Exception>(async () => await _sut.DeleteComentarioVote(comentarioVoteId));
+	}
+
 }
